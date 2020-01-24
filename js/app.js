1afb682eb73910a3033068cda6e4a52a67139832
Satvir <356Sandhu@gmail.com>
2020-01-23 19:06:32 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 /* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
 particlesJS.load(
   "particles-js",
-  "../assets/particlesjs-config.json",
+  "assets/particlesjs-config.json",
   function() {
     console.log("callback - particles.js config loaded");
   }
